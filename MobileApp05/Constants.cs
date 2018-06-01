a1c750636adaa60d43d5334bd0e3de9e2e56f718
taokj <taokung_jaa@hotmail.com>
2018-06-01 04:20:10 +0000

    Add source code.

---
+++ MobileApp05/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MobileApp05
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://mobileapp05.azurewebsites.net";
+	}
+}
+
